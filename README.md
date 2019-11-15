66b47dab546567fda333de69a55dc724bacefed3
Turk <tuckerfiumara@gmail.com>
2019-11-15 16:06:48 -0700

    Added CSS and updated Readme

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
-# train-sched
+# Train-Sheduler
+
+## About 
+This web page allows users to add trains to the scheduler that will show information about the train. It shows when its next arrival is, as well as how many minutes away it is.
+
+## How to use:
+To add a train simply input the trains information and click the submit button at the bottom of the form. When doing so the table above will update and add your train to the list. It will show the train name, its destination, its frequency, the next arrival time and will display how many minutes away it is.
+
+## Check out the web page here:
+https://thy-turk.github.io/train-sched/
+
+![App Screenshot](/assets/images/screenshot.png)
+
+## Tools Used:
+* Google Fonts
+* Html/CSS
+* javascript
+* jquery
+* AJAX
+* Bootstrap 
+* Firebase
--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,3 +1,4 @@
+// Calling to the firebase database
 var firebaseConfig = {
     apiKey: "AIzaSyCX_zw7DR_sY8c_aubbdzOcJu5xrqpv03Y",
     authDomain: "week7-day2-60bed.firebaseapp.com",
@@ -13,6 +14,7 @@ firebase.initializeApp(firebaseConfig);
 
 var database = firebase.database();
 
+// Variable Declaration
 var trainName = "";
 var destination = "";
 var frequency = "";
@@ -20,43 +22,47 @@ var nextArrival = 0;
 var minutesAway = 0;
 var firstTrain = "";
 
-
+// Event for collecting info
 $("#submit-btn").on("click", function() {
     event.preventDefault();
 
+    // Info from from is stored in variables
     trainName = $("#train-name").val().trim();
     destination = $("#destination").val().trim();
     firstTrain = $("#first-train").val().trim();
     frequency = $("#frequency").val().trim();
 
     console.log(trainName, destination, firstTrain, frequency);
+    
+    // Info from form is sent to firebase database
     database.ref().push({
         trainName: trainName,
         destination: destination,
         firstTrain: firstTrain,
         frequency: frequency
     });
-
+    //Clears form
     document.getElementById("trainForm").reset();
 })
 
 database.ref().on("child_added", function(childSnapshot) {
-
+    // Stored into a variable for firebase
     var trainName = childSnapshot.val().trainName;
     var destination = childSnapshot.val().destination;
     var firstTrain = childSnapshot.val().firstTrain;
     var frequency = childSnapshot.val().frequency;
     
-    
+    // Moment.js used for time formatting
     var timeFormat = "HH:mm";
     var trainFormat = moment(firstTrain, timeFormat).subtract(1, "years");
+    
+    // Using moment.js to calculate time differences for our output
     timeDif = moment().diff(trainFormat, "minutes");
     timeRemain = timeDif % frequency;
     minutesAway = frequency - timeRemain;
     nextArrival = moment().add(minutesAway, "minutes").format("hh:mm A")
    
-    
-
+    //Creating rows dynamically and appending them to the table 
     var newRow = $("<tr>");
     newRow.append("<td>" + trainName + "</td>" + "<td>" + destination + "</td>" + "<td>" + frequency + "</td>" + "<td>" + nextArrival + "</td>" + "<td>" + minutesAway + "</td>");
     $(".table").append(newRow);
